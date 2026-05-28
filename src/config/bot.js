cc10a0afb54e14e4bf05caf0106ef7093d2c7d98
phantomreaper33 <brophyrob93@gmail.com>
2026-05-28 15:12:43 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -88,8 +88,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699", 
-      secondary: "#2F3136", 
+      primary: "#66d52f", 
+      secondary: "#66d52f", 
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287", 
